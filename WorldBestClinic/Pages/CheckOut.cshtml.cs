b61c9812b1813717ef86bc06a900508cdc39acf0
Joycemcool <joyceding007@gmail.com>
2023-11-28 04:01:20 -0400

    Init Json

--- WorldBestClinic/Pages/CheckOut.cshtml.cs
+++ WorldBestClinic/Pages/CheckOut.cshtml.cs
@@ -1,12 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using WorldBestClinic.ViewModels;
+using System.Text.Json;
 
 namespace WorldBestClinic.Pages
 {
     public class CheckOutModel : PageModel
     {
+        static readonly HttpClient client = new HttpClient();
+        [BindProperty]
+        public PaymentViewModel Payment { get; set; }
         public void OnGet()
         {
         }
+
+        //    public async Task<IActionResult> OnPostAsync(PaymentViewModel payment)        
+        //    {
+        //        string ShoppingCartMessage = Request.Cookies["ShoppingCart"];
+
+        //        Payment = new PaymentViewModel();
+        //        Payment.products = ShoppingCartMessage;
+
+        //        string jsonPaymentData = System.Text.Json.JsonSerializer.Serialize<PaymentViewModel>(payment);
+        //        string apiUrl = "https://nscc-inet2005-purchase-api.azurewebsites.net/purchase ";
+
+        //        HttpResponseMessage response = await client.GetAsync(apiUrl, jsonPaymentData);
+
+        //        RedirectToPage("/CheckOUt");
+        //    }
     }
 }
---
+++ WorldBestClinic/ViewModels/PaymentViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace WorldBestClinic.ViewModels
+{
+    public class PaymentViewModel
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+
+        public string City { get; set; } = string.Empty;
+
+        public string Province { get; set; } = string.Empty;
+
+        public string PostalCode { get; set; } = string.Empty;
+
+        public string Country { get; set; } = string.Empty;
+        public int CCNumber { get; set; } = 0;
+        public string CCExpiryDate { get; set; } = string.Empty;
+
+        public int Cvv { get; set; } = 0;
+
+        public string products { get; set; } = string.Empty;
+
+    }
+}
